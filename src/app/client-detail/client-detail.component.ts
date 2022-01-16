774d6fac5c2f99eee06bf8e033170930dafefd98
Omri-Gal-Shenhav <shenhav.omri@gmail.com>
2022-01-16 04:35:05 +0200

    Added client details components

--- src/app/client-detail/client-detail.component.ts
+++ src/app/client-detail/client-detail.component.ts
@@ -1,15 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { ClientDetails } from '../models/API_Classes';
 
 @Component({
   selector: 'app-client-detail',
   templateUrl: './client-detail.component.html',
-  styleUrls: ['./client-detail.component.scss']
+  styleUrls: ['./client-detail.component.scss'],
 })
 export class ClientDetailComponent implements OnInit {
+  @Input() details: ClientDetails;
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
