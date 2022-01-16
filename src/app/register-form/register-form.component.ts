ed625a68172252d5ec330291a370299dd0187f93
Omri-Gal-Shenhav <shenhav.omri@gmail.com>
2022-01-16 04:34:46 +0200

    Implemented register form

--- src/app/register-form/register-form.component.ts
+++ src/app/register-form/register-form.component.ts
@@ -1,15 +1,80 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { first } from 'rxjs';
+import { RegisterClient } from '../models/API_Classes';
+import { AuthService } from '../services/auth.service';
+import { ClientService } from '../services/client.service';
 
 @Component({
   selector: 'app-register-form',
   templateUrl: './register-form.component.html',
-  styleUrls: ['./register-form.component.scss']
+  styleUrls: ['./register-form.component.scss'],
 })
 export class RegisterFormComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  registerForm: FormGroup;
+  loading = false;
+  submitted = false;
+  returnUrl: string;
+  error = '';
+  constructor(
+    private formBuilder: FormBuilder,
+    private router: Router,
+    private authService: AuthService,
+    private clientService: ClientService
+  ) {
+    if (this.authService.currentUserValue) {
+      this.router.navigate(['']);
+    }
   }
 
+  ngOnInit(): void {
+    this.registerForm = this.formBuilder.group({
+      username: ['', [Validators.required]],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', Validators.required],
+    });
+
+    this.returnUrl = '/login';
+  }
+
+  get email() {
+    return this.registerForm.get('email');
+  }
+
+  get password() {
+    return this.registerForm.get('password');
+  }
+
+  get username() {
+    return this.registerForm.get('username');
+  }
+
+  onSubmit() {
+    this.submitted = true;
+
+    // stop here if form is invalid
+    if (this.registerForm.invalid) {
+      return;
+    }
+
+    this.loading = true;
+    let client: RegisterClient = {
+      username: this.username.value,
+      password: this.password.value,
+      email: this.email.value,
+    };
+    this.clientService
+      .registerClient(client)
+      .pipe(first())
+      .subscribe(
+        (data) => {
+          this.router.navigate([this.returnUrl]);
+        },
+        (error) => {
+          this.error = error;
+          this.loading = false;
+        }
+      );
+  }
 }
