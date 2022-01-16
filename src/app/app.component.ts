ade9a28229cb09538fc8e57caaa580edb82c2e1d
Omri-Gal-Shenhav <shenhav.omri@gmail.com>
2022-01-16 05:53:55 +0200

    Logout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener, OnDestroy } from '@angular/core';
 import { Router } from '@angular/router';
 import { User } from './models/API_Classes';
 import { AuthService } from './services/auth.service';
@@ -8,16 +8,29 @@ import { AuthService } from './services/auth.service';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnDestroy {
   title = 'My Dashboard';
   currentUser: User;
+
+  @HostListener('window:beforeunload', ['$event'])
+  unloadHandler(event) {
+    this.sendLogout();
+  }
 
   constructor(private router: Router, private authService: AuthService) {
     this.authService.currentUser.subscribe((x) => (this.currentUser = x));
   }
 
-  logout() {
+  ngOnDestroy() {
+    this.sendLogout();
+  }
+
+  clickLogout() {
     this.authService.logout();
     this.router.navigate(['/login']);
+  }
+
+  sendLogout() {
+    if (this.currentUser) this.authService.logout();
   }
 }
