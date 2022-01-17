14f02a6bf8d5d62d78b000fe61048d5b5d76d210
Omri-Gal-Shenhav <shenhav.omri@gmail.com>
2022-01-17 06:37:02 +0200

    Logout bug fix

--- src/app/guards/auth.guard.ts
+++ src/app/guards/auth.guard.ts
@@ -14,6 +14,7 @@ export class AuthGuard implements CanActivate {
   constructor(private router: Router, private authService: AuthService) {
     this.authService.userObservable.subscribe((user) => {
       if (user) this.isLoggedIn = true;
+      else this.isLoggedIn = false;
     });
   }
   canActivate(
